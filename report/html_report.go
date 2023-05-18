f081446a233863ddaf1e7d4390a965983d851b0e
Ramtin <Ramtin.abadi@gmail.com>
2023-05-19 05:21:00 +0530

    Added basic file generation

--- report/html_report.go
+++ report/html_report.go
@@ -16,6 +16,7 @@ import (
 	"runtime"
 	"strings"
 
+	terminalutils "github.com/vieolo/terminal-utils"
 	"golang.org/x/tools/cover"
 )
 
@@ -97,6 +98,19 @@ func GenHTMLReport(outPath string) error {
 			folders = append(folders, thisFolder)
 		} else {
 			folders[index] = thisFolder
+		}
+	}
+
+	outFolder := strings.Replace(outPath, "/coverage.out", "", 1)
+	for _, fol := range folders {
+		for _, file := range fol.Files {
+			sp := strings.Split(file.Name, "/")
+
+			newFileName := fmt.Sprintf("%v/%v.html", outFolder, strings.Replace(sp[len(sp)-1], ".go", ".html", 1))
+			we := os.WriteFile(newFileName, []byte(generateCompleteHTMLFile(string(file.Body))), 0777)
+			if we != nil {
+				terminalutils.PrintError(we.Error())
+			}
 		}
 	}
 
@@ -236,4 +250,66 @@ func getFolderRelativePath(p string) (string, string) {
 	}
 
 	return strings.Join(sp, "/"), sp[len(sp)-1]
+}
+
+func generateCompleteHTMLFile(content string) string {
+	temp := fmt.Sprintf(`
+	<html>
+
+		<head>
+		<style>
+		body {
+			background: black;
+			color: rgb(80, 80, 80);
+		}
+		body, pre, #legend span {
+			font-family: Menlo, monospace;
+			font-weight: bold;
+		}
+		#topbar {
+			background: black;
+			position: fixed;
+			top: 0; left: 0; right: 0;
+			height: 42px;
+			border-bottom: 1px solid rgb(80, 80, 80);
+		}
+		#content {
+			margin-top: 50px;
+		}
+		#nav, #legend {
+			float: left;
+			margin-left: 10px;
+		}
+		#legend {
+			margin-top: 12px;
+		}
+		#nav {
+			margin-top: 10px;
+		}
+		#legend span {
+			margin: 0 5px;
+		}
+		.cov0 { color: rgb(192, 0, 0) }
+		.cov1 { color: rgb(128, 128, 128) }
+		.cov2 { color: rgb(116, 140, 131) }
+		.cov3 { color: rgb(104, 152, 134) }
+		.cov4 { color: rgb(92, 164, 137) }
+		.cov5 { color: rgb(80, 176, 140) }
+		.cov6 { color: rgb(68, 188, 143) }
+		.cov7 { color: rgb(56, 200, 146) }
+		.cov8 { color: rgb(44, 212, 149) }
+		.cov9 { color: rgb(32, 224, 152) }
+		.cov10 { color: rgb(20, 236, 155) }
+
+	</style>
+		</head>
+
+		<body>
+			<pre>%v
+			</pre>
+		</body>
+	</html>
+	`, content)
+
+	return temp
 }
